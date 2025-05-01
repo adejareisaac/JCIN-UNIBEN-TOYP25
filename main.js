8f29b0548e1ea599c0a9ea0299b3ec058f03c4e6
samuelikhifa <signup@ikhifasamuel12.comgit config --global credential.helper cachegit config --global user.email signupsikhifasamuel12.c0m>
2025-05-01 14:05:22 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+// Function to toggle the navbar visibility on mobile
+function toggleMenu() {
+  const nav = document.querySelector("nav");
+  nav.classList.toggle("active"); // Toggle the 'active' class on <nav>
+}
