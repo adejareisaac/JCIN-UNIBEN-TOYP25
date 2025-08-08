37e99741980b7a13fea35ce50f0afd9bbf74179d
Adisaac <isaacadejare438@gmail.com>
2025-08-08 11:03:29 +0100

    Isaac Corrected main.js preloader issue

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ function toggleMenu() {
   const nav = document.querySelector("nav");
   nav.classList.toggle("active"); // Toggle the 'active' class on <nav>
 }
+
+window.addEventListener('load', function() {
+  var preloader = document.getElementById('creative-preloader');
+  if (preloader) {
+    preloader.style.display = 'none';
+  }
+});
